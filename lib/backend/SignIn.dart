6b910e91ffe05558fde15cd9ddde700b1ccb8f43
pratikjain04 <pratikjain0411@gmail.com>
2018-08-16 03:21:50 +0530

    Gradient Texture added in SignIn Page

--- lib/backend/SignIn.dart
+++ lib/backend/SignIn.dart
@@ -6,13 +6,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 import '../home.dart';
 
 void main() => runApp(new MaterialApp(
-  home: new SignIn(),
-  debugShowCheckedModeBanner: false,
-));
+      home: new SignIn(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class SignIn extends StatefulWidget {
-
-
   bool isLogged;
   String text;
 
@@ -22,8 +20,7 @@ class SignIn extends StatefulWidget {
   SignInState createState() => new SignInState();
 }
 
-class SignInState extends State<SignIn>{
-
+class SignInState extends State<SignIn> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = new GoogleSignIn();
 
@@ -32,62 +29,80 @@ class SignInState extends State<SignIn>{
     GoogleSignInAuthentication gSA = await gSI.authentication;
 
     FirebaseUser user = await _auth.signInWithGoogle(
-        idToken: gSA.idToken,
-        accessToken: gSA.accessToken
-    );
-    setState((){
+        idToken: gSA.idToken, accessToken: gSA.accessToken);
+    setState(() {
       widget.isLogged = true;
       widget.text = 'Signed in successfully';
     });
     print("User name: ${user.displayName}");
-    Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Home(isLogged: widget.isLogged, text: widget.text,)));
+    Navigator.of(context).push(new MaterialPageRoute(
+        builder: (BuildContext context) => new Home(
+              isLogged: widget.isLogged,
+              text: widget.text,
+            )));
     return user;
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Sign In Page'),
-          ],
-        )
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Center(
-                child: Container(
-                  width: 330.0,
-                  height: 50.0,
-                  child: RaisedButton(
-                    onPressed: widget.isLogged ? null : () => _signIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)),
-                    color: googleSignInColor,
-                    child: Row(
-                      children: <Widget>[
-                        new Image(image: new AssetImage('images/whitegooglelogo.png'), height: 20.0, width: 20.0),
-                        new Padding(padding: EdgeInsets.all(25.0)),
-                        new Text('Sign In with Google' ,
-                          style: new TextStyle(
+          title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Sign In Page'),
+        ],
+      )),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            gradient: LinearGradient(colors: <Color>[
+              Colors.lightBlueAccent,
+              Colors.lightBlue[400],
+              Colors.blue[600]
+            ])),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Center(
+                  child: Container(
+                    width: 330.0,
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: widget.isLogged
+                          ? null
+                          : () => _signIn()
+                              .then((FirebaseUser user) => print(user))
+                              .catchError((e) => print(e)),
+                      color: googleSignInColor,
+                      child: Row(
+                        children: <Widget>[
+                          new Image(
+                              image:
+                                  new AssetImage('images/whitegooglelogo.png'),
+                              height: 20.0,
+                              width: 20.0),
+                          new Padding(padding: EdgeInsets.all(25.0)),
+                          new Text(
+                            'Sign In with Google',
+                            style: new TextStyle(
                               color: Colors.white,
                               fontSize: 18.0,
-                          ),)
-                      ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Text(widget.text)
-          ],
+              Text(widget.text)
+            ],
+          ),
         ),
       ),
     );
