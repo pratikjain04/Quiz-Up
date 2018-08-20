49e5505dc56a600a8bdd9ea35bb4045c1c8da934
pratikjain04 <pratikjain0411@gmail.com>
2018-08-21 02:14:50 +0530

    HomePage UI update

--- lib/home.dart
+++ lib/home.dart
@@ -2,59 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   HomeState createState() => HomeState();
-
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void _signOut() async{
-    _auth.signOut().whenComplete((){
+  void _signOut() async {
+    _auth.signOut().whenComplete(() {
       googleSignIn.signOut();
     });
-    Navigator.of(context).pushNamedAndRemoveUntil('/SignIn', (Route<dynamic> route) => false);
+    Navigator
+        .of(context)
+        .pushNamedAndRemoveUntil('/SignIn', (Route<dynamic> route) => false);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Container(
-        color: Colors.cyan,
-        padding: new EdgeInsets.all(32.0),
-        child: new Container(
-          child: new Column(
-            children: <Widget>[
-              new Row(
-                children: <Widget>[
-                  new Text(""),
-                ],
-              ),
-              Expanded(
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 50.0,
-                      width: 220.0,
-                      child: RaisedButton(
-                        onPressed: _signOut,
-                        color: Colors.blue,
-                        child: Text('Sign out', style: new TextStyle(color: Colors.white, fontSize: 18.0) ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image(
+            image: new AssetImage('images/bluebrickwall.jpg'),
+            fit: BoxFit.cover,
+
           ),
-        ),
+
+           Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 50.0,
+                  width: 220.0,
+                  child: RaisedButton(
+                    onPressed: _signOut,
+                    color: Colors.blue,
+                    child: Text('Sign Out',
+                        style:
+                            new TextStyle(color: Colors.white, fontSize: 18.0)),
+                  ),
+                ),
+              ],
+            ),
+        ],
       ),
     );
   }
 }
-
-
