568e2ad10d79909421ef92c56c0fc5ce12ce8a95
pratikjain04 <pratikjain0411@gmail.com>
2018-08-21 00:57:02 +0530

    UI update

--- lib/home.dart
+++ lib/home.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Home extends StatefulWidget{
-
+class Home extends StatefulWidget{ 
   @override
   HomeState createState() => HomeState();
 
