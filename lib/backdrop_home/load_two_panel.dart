9fdb2d3e3f1fa083ca1d1528dc7acdc73538fec3
ateek-ujjawal <ateekujjawal@gmail.com>
2018-09-02 01:49:42 +0530

    Add profile page template
    Signed-off-by: ateek-ujjawal <ateekujjawal@gmail.com>

--- lib/backdrop_home/load_two_panel.dart
+++ lib/backdrop_home/load_two_panel.dart
@@ -1,65 +1,37 @@
 import 'package:flutter/material.dart';
 import 'twopanels.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class BackDrop extends StatefulWidget {
   @override
   _BackDropState createState() => _BackDropState();
 }
 
-class _BackDropState extends State<BackDrop> with SingleTickerProviderStateMixin {
-
+class _BackDropState extends State<BackDrop>
+    with SingleTickerProviderStateMixin {
   AnimationController controller;
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  void _signOut() async {
-    _auth.signOut().whenComplete(() {
-      googleSignIn.signOut();
-    });
-    Navigator.of(context).pushNamedAndRemoveUntil('/SignIn', (Route<dynamic> route) => false);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(duration: Duration(milliseconds: 100),vsync: this);
-    controller.fling(velocity: 1.0);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  //checking for backPanel Visibility
-  bool get isPanelVisible {
-    final AnimationStatus status = controller.status;
-    return status == AnimationStatus.completed || status == AnimationStatus.forward;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         backgroundColor: Colors.black54.withOpacity(0.75),
         elevation: 0.0,
         title: Center(child: Text('Quiz-Up')),
         leading: IconButton(
             icon: AnimatedIcon(
-                icon: AnimatedIcons.home_menu,
-                progress: controller.view
-            ),
-            onPressed: (){
-              controller.fling(velocity: isPanelVisible ? -1.0: 1.0);   //1.0 is the default value
+                icon: AnimatedIcons.home_menu, progress: controller.view),
+            onPressed: () {
+              controller.fling(
+                  velocity:
+                      isPanelVisible ? -1.0 : 1.0); //1.0 is the default value
             }),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.exit_to_app), onPressed: _signOut)
         ],
-      ),
-      body: TwoPanels(controller: controller),
+      ),*/
+      body: Stack(children: <Widget>[
+          TwoPanels(),
+      ]),
     );
   }
 }
--- lib/backdrop_home/twopanels.dart
+++ lib/backdrop_home/twopanels.dart
@@ -1,57 +1,197 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:demo_1/ui/home/home_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class TwoPanels extends StatefulWidget{
-
-  final AnimationController controller;
-  TwoPanels({this.controller});
 
   @override
   _TwoPanelsState createState() => _TwoPanelsState();
 }
 
-class _TwoPanelsState extends State<TwoPanels> {
+class _TwoPanelsState extends State<TwoPanels> with SingleTickerProviderStateMixin{
+
+  AnimationController controller;
 
   static const header_height = 52.0;
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void _signOut() async {
+    _auth.signOut().whenComplete(() {
+      googleSignIn.signOut();
+    });
+    Navigator
+        .of(context)
+        .pushNamedAndRemoveUntil('/SignIn', (Route<dynamic> route) => false);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(milliseconds: 100), vsync: this);
+    controller.fling(velocity: 1.0);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  bool get isPanelVisible {
+    final AnimationStatus status = controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
   //For Sliding Effect
 
-  Animation<RelativeRect> getPanelAnimation(BoxConstraints constraints){
+  Animation<RelativeRect> getPanelAnimation(BoxConstraints constraints, double padding){
   //BoxConstraints for taking the complete space of its parent widget
 
     final height = constraints.biggest.height;
-    final backPanelHeight = height - header_height;
+    final backPanelHeight = height - header_height - padding;
     final frontPanelHeight = -header_height;
 
     return RelativeRectTween(
       begin: RelativeRect.fromLTRB(0.0, backPanelHeight, 0.0, frontPanelHeight),
       end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0)
-    ).animate(new CurvedAnimation(parent: widget.controller, curve: Curves.linear));
+    ).animate(new CurvedAnimation(parent: controller, curve: Curves.linear));
   }
 
   Widget bothPanels(BuildContext context, BoxConstraints constraints) {
-    return Container(
-      child: Stack(
+    return Stack(
         children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image(image: AssetImage('images/profile_background.jpg')),
+            ],
+          ),
           Container(
-          color: Colors.black54.withOpacity(0.75),
-          child: Center(
-              child: Text('Profile', style: TextStyle(fontSize: 24.0, color: Colors.white)),
+            child: Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      IconButton(
+                        icon: AnimatedIcon(
+                            icon: AnimatedIcons.home_menu,
+                            progress: controller.view),
+                        onPressed: () {
+                          controller.fling(velocity: isPanelVisible ? -1.0 : 1.0);
+                        },
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 130.0),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Text(
+                          'Profile',
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 108.0, top: 20.0),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.exit_to_app),
+                        onPressed: _signOut,
+                        color: Colors.white,
+                      )
+                    ])),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0, left: 25.0),
+            child: Container(
+              height: 350.0,
+              width: 350.0,
+              child: Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 48.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Pratik Jain', style: TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold),),
+                      Padding(padding: EdgeInsets.only(top: 3.0)),
+                      Text(
+                        'Rookie',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black54.withOpacity(0.70),
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Padding(padding: EdgeInsets.only(left: 40.0)),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 6.0),
+                            child: Text('120', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),),
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 140.0)),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 6.0),
+                            child: Text('T', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),),
+                          ),
+
+                        ],
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemBuilder: (BuildContext context, int index){
+                            return Row(
+                              children: <Widget>[
+                                Text('Normal Icon'),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 40.0),
+                                ),
+                                Text('Current Level: 20'),
+                              ],
+                            );
+                          },
+                          itemCount: 2,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 150.0, top: 135.0),
+            child: ClipOval(
+              child: Container(
+                height: 92.0,
+                width: 92.0,
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage('images/profile_background.jpg'),
+                ),
+              ),
             ),
           ),
           PositionedTransition(
-            rect: getPanelAnimation(constraints),
-            child: Material(
-              elevation: 12.0,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(36.0),
-                topRight: Radius.circular(36.0),
+            rect: getPanelAnimation(constraints, 70.0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 70.0),
+              child: Material(
+                elevation: 12.0,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(36.0),
+                  topRight: Radius.circular(36.0),
+                ),
+                //Front Panel Starts Here
+                child: new HomePage()
               ),
-              //Front Panel Starts Here
-              child: new HomePage()
             ),
           )
         ],
-      ),
     );
   }
 
